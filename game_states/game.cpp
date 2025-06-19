5cc53b76ac9886c5bf6534441052194803315d40
Rafael201007 <87946342+Rafael201007@users.noreply.github.com>
2025-06-19 00:29:35 -0300

    fixing game.cpp

--- game_states/game.cpp
+++ game_states/game.cpp
@@ -1,25 +1,25 @@
 #include <SFML/Window/Mouse.hpp>
-#include "song_select.h"
+#include "game.h"
 #include "../utils/tween_service.h"
 #include "../shaders/shader_manager.h"
 #include "../utils/utilities.h"
 #include "../utils/tween_storage.h"
 #include "../state_stack.h"
 
-SongSelect::SongSelect(StateStack& stack, sf::RenderWindow& window)
+Game::Game(StateStack& stack, sf::RenderWindow& window)
     : State(stack, window)
 {
 
 }
 
-void SongSelect::handleEvent(const sf::Event& event) {
+void Game::handleEvent(const sf::Event& event) {
 
 }
 
-void SongSelect::update(sf::Time dt) {
+void Game::update(sf::Time dt) {
 
 }
 
-void SongSelect::render() {   
-    
+void Game::render() {   
+
 }
